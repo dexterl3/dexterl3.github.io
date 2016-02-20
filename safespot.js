e9df701e541f6d32cf919e303ec94470ec1e3824
Dexter Lesaca III <dexterl3@uw.edu>
2016-02-19 16:51:25 -0800

    add

---
+++ safespot.js
@@ -0,0 +1,21 @@
+// Dexters Maps
+
+
+$(document).ready(function(){
+	initMap();
+});
+
+function initMap() {
+  var myLatLng = {lat: -25.363, lng: 131.044};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: myLatLng
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Hello World!'
+  });
+}
