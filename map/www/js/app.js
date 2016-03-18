4c8cdcd73d70eded037e86fe7f9adbaca6714c34
Dexter Lesaca III <dexterl3@uw.edu>
2016-03-17 20:25:23 -0700

    Updating Site

--- map/www/js/app.js
+++ map/www/js/app.js
@@ -6,7 +6,7 @@
 var nameApp = angular.module('starter', ['ionic', 'uiGmapgoogle-maps']);
  
 nameApp.config(function($stateProvider, $urlRouterProvider) {
-  
+ 
   $stateProvider
     .state('home', {
       url: '/',
@@ -14,10 +14,60 @@ nameApp.config(function($stateProvider, $urlRouterProvider) {
       controller: 'HomeCtrl'
     });
   $urlRouterProvider.otherwise("/");
-  
+ 
 });
  
-
-nameApp.controller('HomeCtrl', function($scope) {
-  $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+ 
+nameApp.controller('HomeCtrl', function($scope, uiGmapGoogleMapApi) {
+ 
+  $scope.myLocation = {
+    lng : '',
+    lat: ''
+  }
+ 
+  $scope.options = {
+    enableHighAccuracy: true,
+    timeout: 50000,
+    maximumAge: 0
+  };
+   
+  $scope.drawMap = function(position) {
+ 
+    //$scope.$apply is needed to trigger the digest cycle when the geolocation arrives and to update all the watchers
+    $scope.$apply(function() {
+      $scope.myLocation.lng = position.coords.longitude;
+      $scope.myLocation.lat = position.coords.latitude;
+ 
+      $scope.map = {
+        center: {
+          latitude: $scope.myLocation.lat,
+          longitude: $scope.myLocation.lng
+        },
+        zoom: 14,
+        pan: 1
+      };
+ 
+      $scope.marker = {
+        id: 0,
+        coords: {
+          latitude: $scope.myLocation.lat,
+          longitude: $scope.myLocation.lng
+        }
+      }; 
+       
+      $scope.marker.options = {
+        draggable: false,
+        labelContent: "lat: " + $scope.marker.coords.latitude + '<br/> ' + 'lon: ' + $scope.marker.coords.longitude,
+        labelAnchor: "80 120",
+        labelClass: "marker-labels"
+      };  
+    });
+  }
+ 
+  $scope.handleError = function(error) {  
+    console.warn('ERROR(' + error.code + '): ' + error.message);
+  }
+ 
+  navigator.geolocation.getCurrentPosition($scope.drawMap, $scope.handleError, $scope.options);  
+ 
 });
